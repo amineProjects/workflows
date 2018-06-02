fd9670b5c6bed2453f0f26f78d8d8697a055ef58
amine <amine31oran2011@gmail.com>
2018-06-02 11:14:57 +0100

    Added coffeeScript files

--- gulpFile.js
+++ gulpFile.js
@@ -1,6 +1,10 @@
 var gulp = require("gulp"),
-    logs = require("fancy-log");
-
-gulp.task("log", function () {
-    logs("a message");
-});
+    logs = require("fancy-log"),
+    coffee=require("gulp-coffee");
+var coffeeSources=["components/coffee/tagline.coffee"]
+gulp.task("coffee", function(){
+gulp.src(coffeeSources)
+    .pipe(coffee({bare:true})
+        .on("error",logs))
+    .pipe(gulp.dest("components/scripts"))
+})
